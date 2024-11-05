6df98d199ab7842715421e762131d7e74528c60b
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2024-11-05 19:43:50 +0100

    pridana funkce EvaluateAndString

--- ukol211024/IExpression.cs
+++ ukol211024/IExpression.cs
@@ -1,7 +1,13 @@
 ﻿
+using System.Linq.Expressions;
+
 public interface IExpression
 {
 	double Evaluate();
 	string GetString();
+	void EvaluateAndString(IExpression expression)
+	{
+		Console.WriteLine($"{expression.GetString()} = {expression.Evaluate()}");
+	}
 }
 
--- ukol211024/Program.cs
+++ ukol211024/Program.cs
@@ -2,12 +2,11 @@
 {
 	static void Main(string[] args)
 	{
-		//(-3+7)*2
 		IExpression expression = new BinaryExpression(new Number(6),
 			new BinaryExpression(new Number(6),
 			new BinaryExpression(new Number(3),new Number(3), "^"), "*")
 													,"+");
-		Console.WriteLine($"{expression.GetString()} = {expression.Evaluate()} ");
+		expression.EvaluateAndString(expression);
 	}
 }
 
