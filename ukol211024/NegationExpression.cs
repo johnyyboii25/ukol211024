d6542f767c6a908b358390ac662ef8fb30755ccd
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2024-11-05 19:24:29 +0100

    final ig

--- ukol211024/NegationExpression.cs
+++ ukol211024/NegationExpression.cs
@@ -1,9 +1,1 @@
-﻿public class NegationExpression : UnaryExpression
-{
-	public NegationExpression(IExpression expression) : base(expression, "-") { }
-
-	public override double Evaluate()
-	{
-		return -_expression.Evaluate();
-	}
-}
+﻿
--- ukol211024/Program.cs
+++ ukol211024/Program.cs
@@ -3,16 +3,11 @@
 	static void Main(string[] args)
 	{
 		//(-3+7)*2
-		IExpression expression = new DivisionExpression(new AdditionExpression(new NegationExpression(new Number(3)),new Number(7)),new Number(2));
-		//[(-5+6.9*8)/8]^4
-		IExpression expression1 = new ExponentiateExpression(
-									new DivisionExpression(
-										new AdditionExpression(new NegationExpression(new Number(5)),
-										new MultiplicationExpresion(new Number(6.9),new Number(8))),
-										new Number(8)),
-										new Number(4));
-		Console.WriteLine($"Výsledek: {expression.Evaluate()}");
-		Console.WriteLine($"Výsledek 2: {expression1.Evaluate()}");
+		IExpression expression = new BinaryExpression(new Number(6),
+			new BinaryExpression(new Number(6),
+			new BinaryExpression(new Number(3),new Number(3), "^"), "*")
+													,"+");
+		Console.WriteLine($"Výsledek: {expression.Evaluate()} ,,,,, {expression.GetString()}");
 	}
 }
 
